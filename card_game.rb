0a03fa081c2c9eacebf07cf03b045d4f780f543a
Steven Loar <sjl7909@gmail.com>
2018-05-23 09:13:01 -0400

    Initialize card game

---
+++ card_game.rb
@@ -0,0 +1,45 @@
+class Card
+  def initialize(suit,rank)
+    @suit = suit
+    @rank = rank
+  end
+
+  def suit
+    @suit
+  end
+
+  def rank
+    @rank
+  end
+end
+
+class Deck
+  def initialize
+    suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+    ranks = ['1', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+    @cards = []
+     suits.each do |suit|
+       ranks.each do |rank|
+         @cards << Card.new(suit,rank)
+       end
+     end
+     @cards.shuffle!
+  end
+
+  def cards
+    @cards
+  end
+end
+
+class Game
+  def initialize
+    @deck = Deck.new
+  end
+
+  def deck
+    @deck
+  end
+end
+
+game = Game.new
+p game.deck.cards[23].suit
